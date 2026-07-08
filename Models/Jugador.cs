c7d8b46073f3ebc9254b7b49a2d4be8b8e6f6054
Fernando Andrés Calderon Muñoz <Fercho_Andres@hotmail.com>
2026-07-07 19:53:30 -0500

    Se añaden validaciones en clase jugador para determinar si son mayores de edad o numeros validos

--- Models/Jugador.cs
+++ Models/Jugador.cs
@@ -6,6 +6,8 @@ namespace TorneoPOO_FCALDERON.Models
 {
     public class Jugador
     {
+        //ATRIBUTOS O CARACTERISTICAS 
+
         public string Nombre { get; set; } 
         public int  Edad {  get; set; }
 
@@ -19,6 +21,26 @@ namespace TorneoPOO_FCALDERON.Models
         {
             Console.WriteLine($"Hola soy {this.Nombre} Tengo {this.Edad} años y mi número es el {this.Numero}");
         }
+
+        public Boolean EsMayorEdad()
+        {
+            if (this.Edad>=18  ) 
+            {
+                return true;
+
+            }
+            else { return false; }
+        }
+
+        public Boolean EsNumeroValido()
+        {
+            if(this.Numero>=0 && this.Numero <100 ) 
+            {
+                return true;
+            }
+        }
+
+
 
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TorneoPOO_FCALDERON
+
+2026-07-02
+Mi Primera Clase
+
+2026-07-07
+Comportamientos de clases
