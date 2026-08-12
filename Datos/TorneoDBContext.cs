2bca0be72ad20bca2d6508380be26be11b00c7e9
Fernando Andrés Calderon Muñoz <Fercho_Andres@hotmail.com>
2026-08-11 22:36:45 -0500

    ok 2

--- Datos/TorneoDBContext.cs
+++ Datos/TorneoDBContext.cs
@@ -16,11 +16,9 @@ namespace TorneoPOO_FCALDERON.Datos
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //CADENA CONEXION USUARIO SQL SERVER
-            optionsBuilder.UseSqlServer("Data Source=torneo.db);
-            //CADENA CONEXION USUARIO SQL SERVER
-
+            optionsBuilder.UseSqlServer("Server=DESKTOP-DQDC13N\SQLEXPRESS;Database=TORNEO_FCALDERON;User Id=sa;Password=1234;TrustServerCertificate=True;");
             //CADENA CONEXION USUARIO WINDOWS
-            optionsBuilder.UseSqlServer("Server=DESKTOP-DQDC13N\SQLEXPRESS;Database=TORNEO_FCALDERON;Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer("Server=DESKTOP-DQDC13N\SQLEXPRESS;Database=TORNEO_FCALDERON;Trusted_Connection=True;");
 
 
         }
